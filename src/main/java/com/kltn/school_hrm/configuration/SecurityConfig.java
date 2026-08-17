3988ce6e5222bc895e9d60fe25d31cbe15786fdf
Bao Duong Q. <dumabao69@gmail.com>
2026-08-17 16:17:13 +0700

    feat(config): implementing security config

---
+++ src/main/java/com/kltn/school_hrm/configuration/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.kltn.school_hrm.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    http
+	        .csrf(csrf -> csrf.disable()) // Tắt CSRF đối với REST API
+	        .authorizeHttpRequests(auth -> auth
+	            // Cho phép truy cập không cần token cho các endpoint này
+	            .requestMatchers("/api/v1/users/register", "/api/v1/auth/login").permitAll()
+	            // Tất cả API khác bắt buộc phải đăng nhập
+	            .anyRequest().authenticated()
+	        );
+	    return http.build();
+	}
+
+}
