ddbebd779ed0bd78923dcb3a82e5101480967be5
Bao Duong Q <dumabao69@gmail.com>
2026-08-25 15:41:41 +0700

    update findExpiringPermitsAndVisas query

--- src/main/java/com/kltn/school_hrm/repository/WorkPermitAndVisaRepository.java
+++ src/main/java/com/kltn/school_hrm/repository/WorkPermitAndVisaRepository.java
@@ -17,9 +17,9 @@ public interface WorkPermitAndVisaRepository extends JpaRepository<WorkPermitAnd
 
 	// Truy vấn các Work Permit / Visa sắp hết hạn trong X ngày (Phục vụ gửi cảnh
 	// báo cho HR)
-	@Query("SELECT w FROM WorkPermitAndVisa w WHERE " +
-			"w.workPermitExpiryDate BETWEEN :today AND :warningDate OR " +
-			"w.visaExpiryDate BETWEEN :today AND :warningDate")
+	@Query("SELECT w FROM WorkPermitAndVisa w" +
+			" WHERE (w.workPermitExpiryDate BETWEEN :today AND :warningDate)" +
+			" OR (w.visaExpiryDate BETWEEN :today AND :warningDate)")
 	List<WorkPermitAndVisa> findExpiringPermitsAndVisas(@Param("today") LocalDate today,
 			@Param("warningDate") LocalDate warningDate);
 }
